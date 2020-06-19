9405a345b2d24c68408b3febada7b40c36211b86
wiarda <wiarda@gmail.com>
2020-06-19 11:58:30 +0800

    Clean up comments and imports

--- app/app.js
+++ app/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { mergeLeft, prop, objOf, compose } = require('ramda');
+const { mergeLeft, objOf, compose } = require('ramda');
 const { 
   setCors,
   parseHeaders,
 } = require('./middleware');
 const sendTemplate = require('./postmark/sendTemplate');
-const validate = require('./validation/validation');
 
 const getEmailField = x => x.email || x.Email;
 const mergeEmailField = requiredFields => compose(mergeLeft(requiredFields), objOf('To'), getEmailField);
@@ -14,23 +13,19 @@ const app = express();
 app.use(setCors, parseHeaders, express.json(), express.urlencoded({ extended: true }));
 
 app.post('/', async (req, res) => {
-  console.log('request received');
-
   try {
-    // get form values
     const formEntries = req.body;
-
-    // convert form's email field into To field that is required by Postmark's template API
     const requiredFields = mergeEmailField(res.locals)(formEntries);
 
-    // validate
+    // TODO: Add server-side form validation
 
     await sendTemplate(requiredFields, formEntries);
-    
+
     res.status(200)
     return res.send({ message: 'OK' });
   } catch (error) {
     console.log(error);
+    // TODO: Split out error types
     return res.send({ status: 400, error })
   }
 });
