d29f24f3438ae6236c1fffeb9b15c9afca5de1f3
wiarda <wiarda@gmail.com>
2020-06-25 09:13:10 +0800

    Add list of valid Postmark body tags

--- app/postmark/sendTemplate.js
+++ app/postmark/sendTemplate.js
@@ -1,6 +1,21 @@
 const client = require('./client');
 const { compose, mergeLeft } = require('ramda');
 
+// List of additional valid body properties for a Postmark sendEmailWithTemplate request
+const BODY_TAGS = [
+  "TemplateAlias",
+  "InlineCss",
+  "Cc",
+  "Bcc",
+  "Tag",
+  "ReplyTo",
+  "Headers",
+  "TrackOpens",
+  "TrackLinks",
+  "Attachments",
+  "Metadata",
+]
+
 /**
  * Combines required template option fields with form-specific fields
  * @param {{}} requiredFields 
